82b29965208b05154cdc2535e72da635c4b26af1
Paul Christian Buhay <61129760+pcbuhay@users.noreply.github.com>
2023-03-05 12:52:35 +0800

    Create game.java

---
+++ game.java
@@ -0,0 +1,134 @@
+import java.awt.Component;
+import java.awt.GridLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+public class stackme  extends JFrame implements KeyListener {
+        int iteration = 1;
+        static double time = 200;
+        static int last = 0;
+        static int m = 10;
+        static int n = 60;
+        JButton b[][];
+        static int length[] = {5,5};
+        static int layer = 55;
+        static int deltax[] = {0,0};
+        static boolean press = false;
+        static boolean forward = true;
+        static boolean start = true;
+        int pointsCount = 0;
+ int timeleft = 59;
+ int counter  = 0;
+        public static void main (String[] args){
+                new stackme();
+        }
+        public stackme(){
+                this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+                b = new JButton [m][n];
+        setLayout(new GridLayout(n,m));
+        for (int y = 0;y<n;y++){
+            for (int x = 0;x<m;x++){
+                    b[x][y] = new JButton(" ");
+                    b[x][y].setBackground(Color.white);
+                    add(b[x][y]);
+                    b[x][y].setEnabled(true);
+            }//end inner for
+    }
+        setFocusable(true);
+        addKeyListener(this);
+        pack();
+        setVisible(true);
+        go();
+        }
+        public void go(){
+                int tmp = 0;
+                Component temporaryLostComponent = null;
+                do{
+                if (forward == true){
+                        forward();
+                } else {
+                        back();
+                }
+                if (deltax[1] == 10-length[1]){
+                        forward = false;
+                } else if (deltax[1] == 0){
+                        forward = true;
+                }
+                draw();
+                try {
+                        Thread.sleep((long) time);
+                } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                }
+                               }while(press == false);
+                if (layer>50){
+                        time= 150-(iteration*iteration*2-iteration);
+                } else {
+                        time = time - 2.2;
+                }
+                iteration++;
+                layer--;
+                press = false;
+                tmp = check();
+                length[0] = length[1];
+                length[1] = tmp;
+                if (layer == -1){
+                        JOptionPane.showMessageDialog(temporaryLostComponent, "IKAWANG HIGHSCORE SALAMA SA PAGLALARO!");
+                }
+                if (length[1] <= 0){   
+                        JOptionPane.showMessageDialog(temporaryLostComponent, "GAME OVER Thank you for playing your score is "+(55-layer)+"!    Email us DreamStarUniverse@yahoo.com ");
+            System.exit(0);
+                }
+                last = deltax[1];
+                start = false;
+                go();
+        }
+        public int check(){
+                if (start == true){
+                        return length[1];
+                } else if (last<deltax[1]){
+                        if (deltax[1]+length[1]-1 <= last+length[0]-1){
+                                return length[1];
+                        } else {
+                                return length[1]-Math.abs((deltax[1]+length[1])-(last+length[0]));
+                        }
+                } else if (last>deltax[1]){
+                        return length[1]-Math.abs(deltax[1]-last);
+                } else {
+                        return length[1];
+                }
+        }
+        public void forward(){
+                deltax[0] = deltax[1];
+                deltax[1]++;
+        }
+        public void back(){
+                deltax[0] = deltax[1];
+                deltax[1]--;
+        }
+        public void draw(){
+                for (int x = 0;x<length[1];x++){
+                        b[x+deltax[0]][layer].setBackground(Color.WHITE);
+                        }
+                for (int x = 0;x<length[1];x++){
+                        b[x+deltax[1]][layer].setBackground(Color.BLACK);
+                        }
+        }
+        @Override
+        public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_SPACE){
+                        press = true;
+                        }
+        }
+        @Override
+        public void keyReleased(KeyEvent arg0) {
+                // TODO Auto-generated method stub
+        }
+        @Override
+        public void keyTyped(KeyEvent arg0) {
+                // TODO Auto-generated method stub
+        }
+}
